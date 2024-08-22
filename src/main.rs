e91882dc75da67e9d3c4523b0623852f4f46e066
Kevin King <kcking@google.com>
2024-08-21 23:15:27 -0700

    espnow example working

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,124 @@
+//! Embassy ESP-NOW Example
+//!
+//! Broadcasts, receives and sends messages via esp-now in an async way
+//!
+//! Because of the huge task-arena size configured this won't work on ESP32-S2
+
+//% FEATURES: async embassy embassy-generic-timers esp-wifi esp-wifi/async esp-wifi/embassy-net esp-wifi/wifi-default esp-wifi/wifi esp-wifi/utils esp-wifi/esp-now
+//% CHIPS: esp32 esp32s3 esp32c2 esp32c3 esp32c6
+
 #![no_std]
 #![no_main]
 
+use embassy_executor::Spawner;
+use embassy_futures::select::{select, Either};
+use embassy_time::{Duration, Ticker};
 use esp_backtrace as _;
 use esp_hal::{
-    clock::ClockControl, delay::Delay, peripherals::Peripherals, prelude::*, system::SystemControl,
+    clock::ClockControl,
+    peripherals::Peripherals,
+    prelude::*,
+    rng::Rng,
+    system::SystemControl,
+    timer::{ErasedTimer, OneShotTimer, PeriodicTimer},
+};
+use esp_println::println;
+use esp_wifi::{
+    esp_now::{PeerInfo, BROADCAST_ADDRESS},
+    initialize, EspWifiInitFor,
 };
 
-extern crate alloc;
-use core::mem::MaybeUninit;
-
-#[global_allocator]
-static ALLOCATOR: esp_alloc::EspHeap = esp_alloc::EspHeap::empty();
-
-fn init_heap() {
-    const HEAP_SIZE: usize = 32 * 1024;
-    static mut HEAP: MaybeUninit<[u8; HEAP_SIZE]> = MaybeUninit::uninit();
-
-    unsafe {
-        ALLOCATOR.init(HEAP.as_mut_ptr() as *mut u8, HEAP_SIZE);
-    }
+// When you are okay with using a nightly compiler it's better to use https://docs.rs/static_cell/2.1.0/static_cell/macro.make_static.html
+macro_rules! mk_static {
+    ($t:ty,$val:expr) => {{
+        static STATIC_CELL: static_cell::StaticCell<$t> = static_cell::StaticCell::new();
+        #[deny(unused_attributes)]
+        let x = STATIC_CELL.uninit().write(($val));
+        x
+    }};
 }
 
-#[entry]
-fn main() -> ! {
-    let peripherals = Peripherals::take();
-    let system = SystemControl::new(peripherals.SYSTEM);
-
-    let clocks = ClockControl::max(system.clock_control).freeze();
-    let delay = Delay::new(&clocks);
-    init_heap();
-
+#[main]
+async fn main(_spawner: Spawner) -> ! {
     esp_println::logger::init_logger_from_env();
 
-    let timer = esp_hal::timer::PeriodicTimer::new(
-        esp_hal::timer::timg::TimerGroup::new(peripherals.TIMG1, &clocks, None)
+    let peripherals = Peripherals::take();
+
+    let system = SystemControl::new(peripherals.SYSTEM);
+    let clocks = ClockControl::max(system.clock_control).freeze();
+
+    let timer = PeriodicTimer::new(
+        esp_hal::timer::timg::TimerGroup::new(peripherals.TIMG0, &clocks, None)
             .timer0
             .into(),
     );
-    let _init = esp_wifi::initialize(
-        esp_wifi::EspWifiInitFor::Wifi,
+
+    let init = initialize(
+        EspWifiInitFor::Wifi,
         timer,
-        esp_hal::rng::Rng::new(peripherals.RNG),
+        Rng::new(peripherals.RNG),
         peripherals.RADIO_CLK,
         &clocks,
     )
     .unwrap();
 
+    let wifi = peripherals.WIFI;
+    let mut esp_now = esp_wifi::esp_now::EspNow::new(&init, wifi).unwrap();
+    println!("esp-now version {}", esp_now.get_version().unwrap());
+
+    #[cfg(feature = "esp32")]
+    {
+        let timg1 = esp_hal::timer::timg::TimerGroup::new(peripherals.TIMG1, &clocks, None);
+        esp_hal_embassy::init(
+            &clocks,
+            mk_static!(
+                [OneShotTimer<ErasedTimer>; 1],
+                [OneShotTimer::new(timg1.timer0.into())]
+            ),
+        );
+    }
+
+    #[cfg(not(feature = "esp32"))]
+    {
+        let systimer = esp_hal::timer::systimer::SystemTimer::new(peripherals.SYSTIMER);
+        esp_hal_embassy::init(
+            &clocks,
+            mk_static!(
+                [OneShotTimer<ErasedTimer>; 1],
+                [OneShotTimer::new(systimer.alarm0.into())]
+            ),
+        );
+    }
+
+    let mut ticker = Ticker::every(Duration::from_secs(5));
     loop {
-        log::info!("Hello world!");
-        delay.delay(500.millis());
+        let res = select(ticker.next(), async {
+            let r = esp_now.receive_async().await;
+            println!("Received {:?}", r);
+            if r.info.dst_address == BROADCAST_ADDRESS {
+                if !esp_now.peer_exists(&r.info.src_address) {
+                    esp_now
+                        .add_peer(PeerInfo {
+                            peer_address: r.info.src_address,
+                            lmk: None,
+                            channel: None,
+                            encrypt: false,
+                        })
+                        .unwrap();
+                }
+                let status = esp_now.send_async(&r.info.src_address, b"Hello Peer").await;
+                println!("Send hello to peer status: {:?}", status);
+            }
+        })
+        .await;
+
+        match res {
+            Either::First(_) => {
+                println!("Send");
+                let status = esp_now.send_async(&BROADCAST_ADDRESS, b"0123456789").await;
+                println!("Send broadcast status: {:?}", status)
+            }
+            Either::Second(_) => (),
+        }
     }
 }
